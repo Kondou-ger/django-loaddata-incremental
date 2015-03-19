59d305990cee7db17a045d421c2997ddece8ba8e
kondou <kondou@ts.unde.re>
2015-03-19 22:39:15 +0100

    Add some more info and use find_packages() in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
     name="loaddata_incremental",
-    packages=["loaddata_incremental"],
+    packages=find_packages(),
     version='0.1',
     description="Load your django fixtures incrementally",
+    license="MIT",
     author="Christopher Schäpers",
     author_email="christopher@schaepers.it",
     url="https://github.com/Kondou-ger/django-loaddata-incremental",
     download_url="https://github.com/Kondou-ger/django-loaddata-incremental/tarball/0.1",
+    install_requires=[
+        'django>=1.6',
+    ],
     keywords=["django", "fixtures"],
-    classifiers=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
